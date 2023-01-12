abb723caabb9c16a11f5c423219798d0a6afe0b5
Dulat <dulatello08@gmail.com>
2023-01-12 20:25:38 +0000

    Pretty much complete assembler. But instruction 0 must be NOP.

---
+++ program-2.s
@@ -0,0 +1,11 @@
+NOP
+STO 0 #1
+STO 1 #1
+.LOOP
+    STM 1 #0
+    STM 0 #1
+    ADR 1 1 #1
+    LDM 0 #0
+    PSH 0
+BRO .LOOP
+HLT
--- program.s
+++ program.s
@@ -1,12 +1,11 @@
-STO 0, 1      ;Initialize memory location 0 to 1
-STO 1, 0      ;Initialize memory location 1 to 0
-STM 1, 0      ;Store the value of memory location 0 in memory location 1
 NOP
-.loop:
-  ADM 0, 1, 1  ;memory location 0 = memory location 1 + memory location 1
-  PSH 1
-  STM 1, 0     ;store the value of memory location 0 in memory location 1
-  ADM 1, 0, 0  ;memory location 1 = memory location 0 + memory location 0
-  NOP ; repeat this loop for as many times as desired before the overflow
-BRO .loop
+STO 0 #1
+STO 1 #0
+STM 1 #0
+.START:
+  ADM 0 1 #1
+  PSH 0
+  STM 1 #0
+  ADM 1 0 #0
+BRO .START
 HLT
