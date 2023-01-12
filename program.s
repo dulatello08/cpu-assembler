879cc4a8ff938f5dbd7f3c75255b3ea1a4f2a371
Dulat <dulatello08@gmail.com>
2023-01-11 19:38:36 -0800

    Some fibonnaci sequence

--- program.s
+++ program.s
@@ -1,7 +1,12 @@
+STO 0, 1      ;Initialize memory location 0 to 1
+STO 1, 0      ;Initialize memory location 1 to 0
+STM 1, 0      ;Store the value of memory location 0 in memory location 1
 NOP
-NOP
-NOP
-.START
-    ADD 0 #1
-    PSH 0
-BRO .START
+.loop:
+  ADM 0, 1, 1  ;memory location 0 = memory location 1 + memory location 1
+  PSH 1
+  STM 1, 0     ;store the value of memory location 0 in memory location 1
+  ADM 1, 0, 0  ;memory location 1 = memory location 0 + memory location 0
+  NOP ; repeat this loop for as many times as desired before the overflow
+BRO .loop
+HLT
