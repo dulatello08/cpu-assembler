ccaae0a940c1b9031576f0050aa31f27df362215
Dulat <dulatello08@gmail.com>
2023-01-22 20:06:00 -0800

    Assembly program

--- wait-for-input.s
+++ wait-for-input.s
@@ -6,7 +6,7 @@ STO 1 #0
 NOP ; Nice routine for byte input.
 PSH 0 ; Print register value, should be replaced with storing to MMIO 0xFF address
 .LOOP
-    SUB 0 #1
     PSH 0
+    SUB 0 #1
     BRO .LOOP
 HLT
