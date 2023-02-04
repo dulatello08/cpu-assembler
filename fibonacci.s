8084a7190b78e912ac574341b2c0653441cd4bd8
Dulat <dulatello08@gmail.com>
2023-02-03 22:25:03 -0800

    I am stuck

--- fibonacci.s
+++ fibonacci.s
@@ -1,13 +1,4 @@
-STO 1 #1        ; Initialize 1 to 1
 .LOOP
     ADD 1 #1
-    NOP ;STM 1 #1
-    NOP ;ADR 1 0 #1      ; R1 = R0 + R1
-    NOP ;STM 1 #1
-    NOP ;ADR 0 0 #1
     STM 1 #FF
-    BRO .OVERFLOW
-    BRN .LOOP
-NOP
-.OVERFLOW
-    HLT
+    BRO .LOOP
