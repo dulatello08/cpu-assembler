05cfa452d85a6da155c1d35557658cc80f5ec6ba
Dulat <dulatello08@gmail.com>
2023-04-16 14:30:50 -0700

    Fully working configurator

--- yaml-configurator/main.c
+++ yaml-configurator/main.c
@@ -7,6 +7,34 @@ typedef struct {
     char *name;
 } Opcode;
 
+// Function to parse a single event and return the value
+static int parse_yaml_event(yaml_parser_t *parser, yaml_event_t *event) {
+    if (!yaml_parser_parse(parser, event)) {
+        printf("Failed to parse event\n");
+        return 0;
+    }
+    return 1;
+}
+
+// Function to parse and handle scalar events
+static int handle_scalar_event(yaml_parser_t *parser, yaml_event_t *event, Opcode *opcodes, int *num_opcodes) {
+    if (strcmp((char *)event->data.scalar.value, "opcode") == 0) {
+        if (!parse_yaml_event(parser, event)) return 0;
+        opcodes[*num_opcodes].opcode = strtol((char *)event->data.scalar.value, NULL, 0);
+    } else if (strcmp((char *)event->data.scalar.value, "name") == 0) {
+        if (!parse_yaml_event(parser, event)) return 0;
+        opcodes[*num_opcodes].name = strdup((char *)event->data.scalar.value);
+    } else if (strcmp((char *)event->data.scalar.value, "num_ops") == 0) {
+        if (!parse_yaml_event(parser, event)) return 0;
+        opcodes[*num_opcodes].num_ops = strtol((char *)event->data.scalar.value, NULL, 0);
+        (*num_opcodes)++;
+    } else if (strcmp((char *)event->data.scalar.value, "op1_mode") == 0) {
+        if (!parse_yaml_event(parser, event)) return 0;
+        opcodes[*(num_opcodes)-1].op1_mode = strtol((char *)event->data.scalar.value, NULL, 0);
+    }
+    return 1;
+}
+
 int main(int argc, char **argv) {
     int option;
     char *inputFileName = NULL;
@@ -58,91 +86,54 @@ int main(int argc, char **argv) {
     int num_opcodes = 0;
     int max_opcodes = 0;
 
-    // Initialize parser
     if (!yaml_parser_initialize(&parser)) {
         printf("Failed to initialize parser\n");
         return 1;
     }
 
-    // Set input file
     yaml_parser_set_input_file(&parser, inputFile);
 
-    // Parse events
     while (1) {
-        if (!yaml_parser_parse(&parser, &event)) {
-            printf("Failed to parse event\n");
-            return 1;
-        }
+        if (!parse_yaml_event(&parser, &event)) return 1;
 
         if (event.type == YAML_SCALAR_EVENT) {
-            if (strcmp((char *)event.data.scalar.value, "opcode") == 0) {
-                // Opcode scalar found, read next event for opcode value
-                if (!yaml_parser_parse(&parser, &event)) {
-                    printf("Failed to parse event\n");
+            if (num_opcodes >= max_opcodes) {
+                max_opcodes = max_opcodes == 0 ? 1 : max_opcodes * 2;
+                opcodes = realloc(opcodes, max_opcodes * sizeof(Opcode));
+                if (!opcodes) {
+                    printf("Failed to allocate memory\n");
                     return 1;
                 }
-                if (num_opcodes >= max_opcodes) {
-                    // Resize opcodes array if necessary
-                    max_opcodes = max_opcodes == 0 ? 1 : max_opcodes * 2;
-                    opcodes = realloc(opcodes, max_opcodes * sizeof(Opcode));
-                    if (!opcodes) {
-                        printf("Failed to allocate memory\n");
-                        return 1;
-                    }
-                }
-                opcodes[num_opcodes].opcode = strtol((char *)event.data.scalar.value, NULL, 0);
-            } else if (strcmp((char *)event.data.scalar.value, "name") == 0) {
-                // Num_ops scalar found, read next event for num_ops value
-                if (!yaml_parser_parse(&parser, &event)) {
-                    printf("Failed to parse event\n");
-                    return 1;
-                }
-                opcodes[num_opcodes].name = (char *)event.data.scalar.value;
-                num_opcodes++;
-            } else if (strcmp((char *)event.data.scalar.value, "num_ops") == 0) {
-                // Num_ops scalar found, read next event for num_ops value
-                if (!yaml_parser_parse(&parser, &event)) {
-                    printf("Failed to parse event\n");
-                    return 1;
-                }
-                opcodes[num_opcodes-1].num_ops = strtol((char *)event.data.scalar.value, NULL, 0);
-            } else if (strcmp((char *)event.data.scalar.value, "op1_mode") == 0) {
-                // Num_ops scalar found, read next event for num_ops value
-                if (!yaml_parser_parse(&parser, &event)) {
-                    printf("Failed to parse event\n");
-                    return 1;
-                }
-                opcodes[num_opcodes-1].op1_mode = strtol((char *)event.data.scalar.value, NULL, 0);
             }
+            if (!handle_scalar_event(&parser, &event, opcodes, &num_opcodes)) return 1;
         }
 
-        if (event.type == YAML_STREAM_END_EVENT) {
-            // End of stream, break out of loop
-            break;
-        }
+        if (event.type == YAML_STREAM_END_EVENT) break;
 
-        // Free event
         yaml_event_delete(&event);
     }
 
-    // Print opcodes
-    uint8_t *outputArray = malloc(num_opcodes*3 * sizeof(uint8_t));
+    // Print opcodes and write to output file
+    uint8_t *outputArray = malloc(num_opcodes * 6 * sizeof(uint8_t));
     for (int i = 0; i < num_opcodes; i++) {
-        printf("Opcode: 0x%02X, Num_ops: %d, Op2_mode: %d\n", opcodes[i].opcode, opcodes[i].num_ops, opcodes[i].op1_mode);
-        outputArray[i] = opcodes[i].opcode;
-        outputArray[i+1] = opcodes[i].name[0];
-        outputArray[i+2] = opcodes[i].name[1];
-        outputArray[i+3] = opcodes[i].name[2];
-        outputArray[i+5] = opcodes[i].num_ops;
-        outputArray[i+6] = opcodes[i].op1_mode;
+        printf("%s: 0x%02X, Num_ops: %d, Op1_mode: %d\n", opcodes[i].name, opcodes[i].opcode, opcodes[i].num_ops, opcodes[i].op1_mode);
+        outputArray[i * 6] = opcodes[i].opcode;
+        outputArray[i * 6 + 1] = opcodes[i].name[0];
+        outputArray[i * 6 + 2] = opcodes[i].name[1];
+        outputArray[i * 6 + 3] = opcodes[i].name[2];
+        outputArray[i * 6 + 4] = opcodes[i].num_ops;
+        outputArray[i * 6 + 5] = opcodes[i].op1_mode;
     }
-    fwrite(outputArray, sizeof(uint8_t), num_opcodes*3, outputFile);
+    fwrite(outputArray, sizeof(uint8_t), num_opcodes * 6, outputFile);
     free(outputArray);
 
     // Cleanup
     yaml_parser_delete(&parser);
     fclose(inputFile);
     fclose(outputFile);
+    for (int i = 0; i < num_opcodes; i++) {
+        free(opcodes[i].name);
+    }
     free(opcodes);
     return 0;
 }
