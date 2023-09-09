d771f0857a3f45f3feaf5f7dbe9810bfe0f4b192
Dulat S <dulatello08@gmail.com>
2023-09-09 14:18:23 -0700

    working fibonacci, todo fix shifted labels

--- programs/fibonacci.s
+++ programs/fibonacci.s
@@ -1,17 +1,17 @@
 STO 0 0
 STO 1 #1
-STO 3 #a
+STO 3 #11
 STM 0 #ff
 STM 1 #ff
 .LOOP
-    ADM 0 1 #100
+    ADR 0 1 #100
     LDM 2 #100
     STM 2 #ff
     PSH 1
     POP 0
-    PSH 3
+    PSH 2
     POP 1
     SUB 3 #1
-    BRZ #1F
-    BRN .LOOP
+    BRZ #2F
+    BRN #12
 HLT
