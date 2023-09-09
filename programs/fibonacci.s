f3a3accdaff31dac812c3fcf94ef4932afec15fe
Dulat S <dulatello08@gmail.com>
2023-09-08 22:31:58 -0700

    shifted labels, weird stuff

--- programs/fibonacci.s
+++ programs/fibonacci.s
@@ -1,5 +1,17 @@
+STO 0 0
+STO 1 #1
+STO 3 #a
+STM 0 #ff
+STM 1 #ff
 .LOOP
-    ADD 1 #1
-    STM 1 #1234
-    BRO .LOOP
+    ADM 0 1 #100
+    LDM 2 #100
+    STM 2 #ff
+    PSH 1
+    POP 0
+    PSH 3
+    POP 1
+    SUB 3 #1
+    BRZ #1F
+    BRN .LOOP
 HLT
