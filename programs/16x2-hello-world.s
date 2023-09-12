bfbe23ada1e75890c73c6aa29aecef1da8f72493
Dulat S <dulatello08@gmail.com>
2023-09-12 13:00:12 -0700

    complex text reader app 39 bytes

--- programs/16x2-hello-world.s
+++ programs/16x2-hello-world.s
@@ -2,12 +2,14 @@ NOP
 STO 0 #0
 STO 1 #0
 STO 2 #f0
+STO 4 #20
 .LOOP
     PSH 1
     PSH 2
     RLD 3
-    BRR 0 3 #1f
+    BRR 0 3 #26
     STM 3 #eff7
     ADD 1 #1
-    BRN #b
+    BRR 1 4 #26
+    BRN #e
 HLT
