3e174d4a6a5c3e6987d47f1e95d0360586d2fda8
Dulat S <dulatello08@gmail.com>
2024-01-30 21:41:56 -0800

    working on fixed point multiplication with new MULL opcode

--- programs/fahrenheit.s
+++ programs/fahrenheit.s
@@ -1,1 +1,6 @@
-MULL 0 1 2 3
+NOP ; estimate celsius from fahrenheit temp.
+STO 0 #64
+SUB 0 #20
+STO 1 #6f
+MULL 0 1 0 1
+; here 0 is lower part of result of mult while 1 is higher part. in total they would be 16 bit int but they are in separate registers. i need to perform right shift on that 16 bit int but I have RSH opcode that can only perform shift on 1 register
