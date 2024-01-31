b3ae07a37dc6a8eb164106d73aecfced605f748a
Dulat <dulatello08@gmail.com>
2024-01-31 00:53:43 +0000

    test program bit shift

--- programs/test.s
+++ programs/test.s
@@ -1,8 +1,10 @@
-NOP
+NOP ; Logical bit shift and bitwise and and or
+STO 0 #35
+STO 1 #20
+STO 2 #8b
+STO 3 #8
 STM 0 #ff
-STO 1 #0
-STM 1 #ff
-JSR #effc
+LSR 0 1 #fe
+LDM 0 #fe
 STM 0 #ff
-STM 1 #ff
 HLT
