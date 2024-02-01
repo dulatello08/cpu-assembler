ff1b2d85292dabde32be57358635e7e4c93e06ea
Dulat S <dulatello08@gmail.com>
2024-02-01 07:39:11 -0800

    very basic approach for division by 1024

--- programs/fahrenheit.s
+++ programs/fahrenheit.s
@@ -3,4 +3,7 @@ STO 0 #64
 SUB 0 #20
 STO 1 #6f
 MULL 0 1 0 1
-; here 0 is lower part of result of mult while 1 is higher part. in total they would be 16 bit int but they are in separate registers. i need to perform right shift on that 16 bit int but I have RSH opcode that can only perform shift on 1 register
+RSH 1 #2
+MUL 1 #5 ;should have result here now
+STM 1 #ff
+HLT
